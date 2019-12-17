0e2ac1f46f61e7dff9d293a3da712b25c9d62588
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-12-17 16:30:02 +0000

    Added some to-dos for next time, added an alert to scripts.js, and added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Jack's Noughts and Crosses Game!
+
+- HTML
+- CSS
+- Bootstrap
+- JavaScript (arrays, variables, DOM manipulation)
--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
+alert('Connected!');
 
-// to-do: push to GitHub for Jack
+
+// to-do: use getElementsByClassName() method to grab all the HTML elements that have a
+// class of 'box'
+
+// put these elements in a variable which will be an array
+
+// manipulate specific items in that array (specific boxes on the board) with CSS executed via JavaScript!
